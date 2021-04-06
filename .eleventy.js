ed470d4e746840ed2276fe0a9a992cd36b70dc53
Viktor Osipov <ipovos@pm.me>
2021-04-06 23:00:41 +0300

    feat: chaptersSortedByNumber

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,18 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addCollection(
     'articlesSortedByNumber',
     function (collectionApi) {
-      return collectionApi.getFilteredByTag('article').sort(function (a, b) {
-        return a.data.number - b.data.number;
-      });
+      return collectionApi
+        .getFilteredByTag('article')
+        .sort((a, b) => a.data.number - b.data.number);
+    },
+  );
+
+  eleventyConfig.addCollection(
+    'chaptersSortedByNumber',
+    function (collectionApi) {
+      return collectionApi
+        .getFilteredByTag('chapter')
+        .sort((a, b) => a.data.number - b.data.number);
     },
   );
 };
