f78f256037a154e694496dee6b7fcd51a22c6d6d
Viktor Osipov <ipovos@pm.me>
2021-04-24 18:58:20 +0300

    feat: about text

--- pages/about.11ty.js
+++ pages/about.11ty.js
@@ -1,4 +1,4 @@
-const {h1, main, p} = require('../components/html');
+const {h1, main, p, a} = require('../components/html');
 const {TopNav} = require('../components/TopNav');
 const {render} = require('../components/render');
 const {createPageTitle} = require('./utils');
@@ -22,7 +22,39 @@ class About {
         children: [
           h1({class: 'tc', children: 'Про сайт'}),
           p({
-            children: 'Подумаю потім',
+            children: 'Для розвитку України потрібно розвивати українців.',
+          }),
+          p({
+            children:
+              'Виховання правової культури — важлива частина зони росту громадян України.',
+          }),
+          p({
+            children:
+              'Цей сайт має на меті покращити знання Конституції України серед усіх, хто знає українську мову, завдяки релевантній пошукової видачі, швидкості відкриття та зручності користування.',
+          }),
+          p({
+            children: [
+              'Джерело тексту Конституціі — ',
+              a({
+                href:
+                  'https://zakon.rada.gov.ua/laws/show/254%D0%BA/96-%D0%B2%D1%80#Text',
+                children:
+                  'Документ 254к/96-ВР, чинний, поточна редакція — Редакція від 01.01.2020, підстава - 27-IX',
+              }),
+            ],
+          }),
+          p({
+            children: [
+              'Контактна пошта: ',
+              a({
+                href: 'mailto:contact@constitution.in.ua',
+                children: 'contact@constitution.in.ua',
+              }),
+            ],
+          }),
+          p({
+            children:
+              'На фавіконці шрифт Source Sans Pro <br><em>Copyright 2010, 2012, 2014 Adobe Systems Incorporated (<a href="http://www.adobe.com/">http://www.adobe.com/</a>), with Reserved Font Name Source. SIL Open Font License, 1.1 (<a href="http://scripts.sil.org/OFL">http://scripts.sil.org/OFL</a>)</em>',
           }),
         ],
       }),
